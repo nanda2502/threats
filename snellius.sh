3ac1b494f98a1c1e557eb6c7060dc4d030692ce5
Nanda Jafarian <n.r.jafarian@student.vu.nl>
2024-05-02 14:01:45 +0200

    changed script to show cont strats above pun strats

--- snellius.sh
+++ snellius.sh
@@ -2,7 +2,7 @@
 #SBATCH -J exp2_c
 #SBATCH -t 10:00
 #SBATCH -p thin
-#SBATCH -n 105
+#SBATCH -n 90
 
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
@@ -10,7 +10,7 @@ module load Python/3.11.3-GCCcore-12.3.0
 SCRIPT="$HOME/home/main2PG_condor.py"
 
 
-for threat in $(seq 0 5 30); do
+for threat in $(seq 35 5 60); do
     for probability in 0.1 0.3 0.5 0.7 0.9; do
         for replication in 1 2 3; do
             python $SCRIPT $threat $probability $replication &
